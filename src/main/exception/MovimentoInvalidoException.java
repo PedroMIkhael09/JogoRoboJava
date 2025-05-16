5c97a677e97aec917c904a8639d190a56f15ff04
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-05-16 18:38:31 -0300

    Criei as classe e implementei algumas logicas

---
+++ src/main/exception/MovimentoInvalidoException.java
@@ -0,0 +1,6 @@
+public class MovimentoInvalidoException extends Exception {
+	
+	public MovimentoInvalidoException(String direcao) {
+		super("Movimento inválido: " + direcao + ". O robô não pode ir para posições negativas.");
+	}
+}
---
+++ src/main/robos/Robo.java
@@ -0,0 +1,123 @@
+public class Robo {
+	protected String cor;
+	protected int posicaoX;
+	protected int posicaoY;
+	protected int movimentosValidos;
+	protected int movimentosInvalidos;
+	
+	public Robo(String cor){
+		this.cor = cor;
+		this.posicaoX = 0;
+		this.posicaoY = 0;
+	}
+	
+	public boolean moverRobo(String direcao) throws MovimentoInvalidoException {
+		if (direcao.equals("up")) {
+			posicaoY++;
+			movimentosValidos++;
+			return true;
+		} else if (direcao.equals("down")) {
+			if (posicaoY - 1 < 0) {
+				movimentosInvalidos++;
+				throw new MovimentoInvalidoException("down");
+			}
+			posicaoY--;
+			movimentosValidos++;
+			return true;
+		} else if (direcao.equals("right")) {
+			posicaoX++;
+			movimentosValidos++;
+			return true;
+		} else if (direcao.equals("left")) {
+			if (posicaoX - 1 < 0) {
+				movimentosInvalidos++;
+				throw new MovimentoInvalidoException("left");
+			}
+			posicaoX--;
+			movimentosValidos++;
+			return true;
+		} else {
+			movimentosInvalidos++;
+			return false;
+		}
+	}
+	
+	public boolean moverRobo(int numero) throws MovimentoInvalidoException {
+		if (numero == 1) { // up
+			posicaoY++;
+			movimentosValidos++;
+			return true;
+		} else if (numero == 2) { // down
+			if (posicaoY - 1 < 0) {
+				movimentosInvalidos++;
+				throw new MovimentoInvalidoException("2 (down)");
+			}
+			posicaoY--;
+			movimentosValidos++;
+			return true;
+		} else if (numero == 3) { // right
+			posicaoX++;
+			movimentosValidos++;
+			return true;
+		} else if (numero == 4) { // left
+			if (posicaoX - 1 < 0) {
+				movimentosInvalidos++;
+				throw new MovimentoInvalidoException("4 (left)");
+			}
+			posicaoX--;
+			movimentosValidos++;
+			return true;
+		} else {
+			movimentosInvalidos++;
+			return false;
+		}
+	}
+	
+	
+	
+	public boolean encontrarAlimento(int posicaoXAlimento, int posicaoYAlimento){
+		if (posicaoXAlimento == posicaoX && posicaoYAlimento == posicaoY){
+			return true;
+		}
+		return false;
+	}
+	
+	public int getPosicaoX() {
+		return posicaoX;
+	}
+	public void setPosicaoX(int posicaoX) {
+		this.posicaoX = posicaoX;
+	}
+	
+	public int getPosicaoY() {
+		return posicaoY;
+	}
+	
+	public void setPosicaoY(int posicaoY) {
+		this.posicaoY = posicaoY;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public int getMovimentosValidos() {
+		return movimentosValidos;
+	}
+	
+	public void setMovimentosValidos(int movimentosValidos) {
+		this.movimentosValidos = movimentosValidos;
+	}
+	
+	public int getMovimentosInvalidos() {
+		return movimentosInvalidos;
+	}
+	
+	public void setMovimentosInvalidos(int movimentosInvalidos) {
+		this.movimentosInvalidos = movimentosInvalidos;
+	}
+	
+}
