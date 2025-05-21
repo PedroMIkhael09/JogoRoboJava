f85f423ba8bb1b93a151cf9061013f503a5970fd
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-05-21 19:17:48 -0300

    Fiz alterações e a perfeiçoei a classe dos robos

--- src/java/main/robos/Robo.java
+++ src/java/main/robos/Robo.java
@@ -1,125 +1,117 @@
 package robos;
+
 import exception.MovimentoInvalidoException;
 
-public class Robo {
+public abstract class Robo {
 	protected String cor;
 	protected int posicaoX;
 	protected int posicaoY;
 	protected int movimentosValidos;
 	protected int movimentosInvalidos;
+	protected String ultimaDirecaoUsada;
 	
-	public Robo(String cor){
+	public Robo(String cor) {
 		this.cor = cor;
 		this.posicaoX = 0;
 		this.posicaoY = 0;
 	}
 	
-	public boolean moverRobo(String direcao) throws MovimentoInvalidoException {
-		if (direcao.equals("up")) {
-			posicaoY++;
-			movimentosValidos++;
-			return true;
-		} else if (direcao.equals("down")) {
-			if (posicaoY - 1 < 0) {
-				movimentosInvalidos++;
-				throw new MovimentoInvalidoException("down");
-			}
-			posicaoY--;
-			movimentosValidos++;
-			return true;
-		} else if (direcao.equals("right")) {
-			posicaoX++;
-			movimentosValidos++;
-			return true;
-		} else if (direcao.equals("left")) {
-			if (posicaoX - 1 < 0) {
-				movimentosInvalidos++;
-				throw new MovimentoInvalidoException("left");
-			}
-			posicaoX--;
-			movimentosValidos++;
-			return true;
-		} else {
-			movimentosInvalidos++;
-			return false;
-		}
-	}
+	public abstract boolean moverRobo(String direcao) throws MovimentoInvalidoException;
 	
-	public boolean moverRobo(int numero) throws MovimentoInvalidoException {
-		if (numero == 1) {
-			posicaoY++;
-			movimentosValidos++;
-			return true;
-		} else if (numero == 2) {
-			if (posicaoY - 1 < 0) {
-				movimentosInvalidos++;
-				throw new MovimentoInvalidoException("2 (down)");
-			}
-			posicaoY--;
-			movimentosValidos++;
-			return true;
-		} else if (numero == 3) {
-			posicaoX++;
-			movimentosValidos++;
-			return true;
-		} else if (numero == 4) {
-			if (posicaoX - 1 < 0) {
-				movimentosInvalidos++;
-				throw new MovimentoInvalidoException("4 (left)");
-			}
-			posicaoX--;
-			movimentosValidos++;
-			return true;
-		} else {
-			movimentosInvalidos++;
-			return false;
-		}
-	}
-	
+	public abstract boolean moverRobo(int numero) throws MovimentoInvalidoException;
 	
 	public boolean encontrarAlimento(int posicaoXAlimento, int posicaoYAlimento){
-		if (posicaoXAlimento == posicaoX && posicaoYAlimento == posicaoY){
+		if (posicaoX == posicaoXAlimento && posicaoY == posicaoYAlimento) {
 			return true;
 		}
 		return false;
 	}
 	
+	protected boolean moverParaDirecao(String direcao) throws MovimentoInvalidoException {
+		switch (direcao) {
+			case "up":
+				posicaoY++;
+				movimentosValidos++;
+				ultimaDirecaoUsada = "up";
+				return true;
+			case "down":
+				if (posicaoY - 1 < 0) {
+					movimentosInvalidos++;
+					throw new MovimentoInvalidoException("down");
+				}
+				posicaoY--;
+				movimentosValidos++;
+				ultimaDirecaoUsada = "down";
+				return true;
+			case "right":
+				posicaoX++;
+				movimentosValidos++;
+				ultimaDirecaoUsada = "right";
+				return true;
+			case "left":
+				if (posicaoX - 1 < 0) {
+					movimentosInvalidos++;
+					throw new MovimentoInvalidoException("left");
+				}
+				posicaoX--;
+				movimentosValidos++;
+				ultimaDirecaoUsada = "left";
+				return true;
+			default:
+				movimentosInvalidos++;
+				return false;
+		}
+	}
+	
+	
+	public String getCor() {
+		return cor;
+	}
+	
 	public int getPosicaoX() {
 		return posicaoX;
-	}
-	public void setPosicaoX(int posicaoX) {
-		this.posicaoX = posicaoX;
 	}
 	
 	public int getPosicaoY() {
 		return posicaoY;
 	}
 	
-	public void setPosicaoY(int posicaoY) {
-		this.posicaoY = posicaoY;
-	}
-	
-	public String getCor() {
-		return cor;
-	}
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
-	
 	public int getMovimentosValidos() {
 		return movimentosValidos;
-	}
-	
-	public void setMovimentosValidos(int movimentosValidos) {
-		this.movimentosValidos = movimentosValidos;
 	}
 	
 	public int getMovimentosInvalidos() {
 		return movimentosInvalidos;
 	}
 	
+	public String getUltimaDirecaoUsada() {
+		return ultimaDirecaoUsada;
+	}
+	
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void setPosicaoX(int posicaoX) {
+		this.posicaoX = posicaoX;
+	}
+	
+	public void setPosicaoY(int posicaoY) {
+		this.posicaoY = posicaoY;
+	}
+	
+	public void setMovimentosValidos(int movimentosValidos) {
+		this.movimentosValidos = movimentosValidos;
+	}
+	
 	public void setMovimentosInvalidos(int movimentosInvalidos) {
 		this.movimentosInvalidos = movimentosInvalidos;
 	}
+	
+	public void setUltimaDirecaoUsada(String ultimaDirecaoUsada) {
+		this.ultimaDirecaoUsada = ultimaDirecaoUsada;
+	}
+	
 	
 }
